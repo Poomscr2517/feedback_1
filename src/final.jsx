e36e12c56556d8ca6c17e80d1c156e90166e1d6d
Poompozhilan T <102795399+Poomscr2517@users.noreply.github.com>
2023-07-27 17:29:16 +0530

    Add files via upload

---
+++ src/final.jsx
@@ -0,0 +1,10 @@
+function FinalPage() {
+  return (
+    <div style={{ textAlign: "center", fontWeight: "50px" }}>
+      <h1>Thanks for your valuable Feedback</h1>
+      Any queries contact to xyz@gmail.com
+    </div>
+  );
+}
+
+export default FinalPage;
